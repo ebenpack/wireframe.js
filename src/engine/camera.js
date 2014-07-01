9706f652442674a2213bc6a655e4193d65f820ee
ebenpack <ebenpackwood@gmail.com>
2014-07-01 14:24:26 -0400

    Add camera movement methods

--- src/engine/camera.js
+++ src/engine/camera.js
@@ -8,12 +8,11 @@ var Matrix = math.Matrix;
  * @param {Vector} position Camera position.
  * @param {Vector} target   Camera
  */
-function Camera(width, height, position, target){
+function Camera(width, height, position){
     this.position = position || new Vector(1,1,20);
-    this.target = target || new Vector(0,0,1);
     this.up = new Vector(0, 1, 0);
     this.rotation = {'yaw': 0, 'pitch': 0, 'roll': 0};
-    this.view_matrix = this.lookAt();
+    this.view_matrix = this.createViewMatrix();
     this.width = width;
     this.height = height;
     this.near = 0.1;
@@ -21,9 +20,19 @@ function Camera(width, height, position, target){
     this.fov = 90;
     this.perspectiveFov = this.calculatePerspectiveFov();
 }
+/**
+ * Calculate the point the camera is looking at.
+ * @return {[type]} [description]
+ */
+Camera.prototype.lookAt = function() {
+    var rotation = Matrix.rotationZ(this.rotation.yaw);
+    var transformed = this.position.transform(rotation);
+    return this.position.add(transformed);
+};
 /** @method */
 Camera.prototype.direction = function() {
-    return this.target.subtract(this.position);
+    var look_at = this.lookAt().normalize();
+    return this.position.subtract(look_at);
 };
 /**
  * Builds a perspective projection matrix based on a field of view.
@@ -48,114 +57,76 @@ Camera.prototype.calculatePerspectiveFov = function() {
     return matrix;
 };
 /** @method */
-Camera.prototype.lookAt = function(){
+Camera.prototype.createViewMatrix = function(){
     var eye = this.position;
-    var target = this.direction();
-    var up = this.up;
-    var zaxis = eye.subtract(target).normalize();
-    var xaxis = up.cross(zaxis).normalize();
-    var yaxis = zaxis.cross(xaxis);
+    var pitch = this.rotation.pitch;
+    var yaw = this.rotation.yaw;
+    var cosPitch = Math.cos(pitch);
+    var sinPitch = Math.sin(pitch);
+    var cosYaw = Math.cos(yaw);
+    var sinYaw = Math.sin(yaw);
+
+    var xaxis = new Vector(cosYaw, 0, -sinYaw );
+    var yaxis = new Vector(sinYaw * sinPitch, cosPitch, cosYaw * sinPitch );
+    var zaxis = new Vector(sinYaw * cosPitch, -sinPitch, cosPitch * cosYaw );
+
+    // Create a 4x4 view matrix from the right, up, forward and eye position vectors
     var view_matrix = Matrix.fromArray([
         xaxis.x, yaxis.x, zaxis.x, 0,
         xaxis.y, yaxis.y, zaxis.y, 0,
         xaxis.z, yaxis.z, zaxis.z, 0,
-        -(xaxis.dot(eye)), -(yaxis.dot(eye)), -(zaxis.dot(eye)), 1
-        ]);
+        -(xaxis.dot(eye) ), -( yaxis.dot(eye) ), -( zaxis.dot(eye) ), 1
+    ]);
     return view_matrix;
 };
-// Vector3 dirVector = Vector3(0.0, 0.0, -1.0);
-// dirVector = scene->getDefaultCamera()->getConcatenatedMatrix().rotateVector(dirVector);
-// Vector3 newPosition = scene->getDefaultCamera()->getPosition() + (dirVector * moveSpeed * elapsed);
 /** @method */
 Camera.prototype.moveTo = function(x, y, z){
-    this.position.x = x;
-    this.position.y = y;
-    this.position.z = z;
-    this.view_matrix = this.lookAt();
+    this.position = new Vector(x,y,z);
+    this.view_matrix = this.createViewMatrix();
 };
 /** @method */
 Camera.prototype.moveRight = function(amount){
-    // TODO: FIX, WRONG
-    var right = this.up.cross(this.direction().normalize().scale(amount));
+    var right = this.up.cross(this.direction()).normalize().scale(amount);
     this.position = this.position.add(right);
-    //this.direction().x += amount;
-    this.view_matrix = this.lookAt();
+    this.view_matrix = this.createViewMatrix();
 };
 /** @method */
 Camera.prototype.moveLeft = function(amount){
-    // TODO: FIX, WRONG
-    var left = this.up.cross(this.direction().normalize().scale(amount));
+    var left = this.up.cross(this.direction()).normalize().scale(amount);
     this.position = this.position.subtract(left);
-    //this.direction().x -= amount;
-    this.view_matrix = this.lookAt();
+    this.view_matrix = this.createViewMatrix();
+};
+Camera.prototype.turnRight = function(amount){
+    this.rotation.yaw += amount;
+    this.view_matrix = this.createViewMatrix();
+};
+/** @method */
+Camera.prototype.turnLeft = function(amount){
+    this.rotation.yaw -= amount;
+    this.view_matrix = this.createViewMatrix();
 };
 /** @method */
 Camera.prototype.moveUp = function(amount){
-    // TODO: WRONG!!!
-    this.position = this.position.add(this.up.normalize().scale(amount));
-    //this.direction().y -= amount;
-    this.view_matrix = this.lookAt();
+    var up = this.up.normalize().scale(amount);
+    this.position = this.position.add(up);
+    this.view_matrix = this.createViewMatrix();
 };
 /** @method */
 Camera.prototype.moveDown = function(amount){
-    // TODO: WRONG!!!
-    this.position = this.position.subtract(this.up.normalize().scale(amount));
-    //this.direction().y += amount;
-    this.view_matrix = this.lookAt();
+    var down = this.up.normalize().scale(amount);
+    this.position = this.position.subtract(down);
+    this.view_matrix = this.createViewMatrix();
 };
 /** @method */
 Camera.prototype.moveForward = function(amount){
-    // TODO: WRONG!!!
-    this.position = this.position.add(this.direction().normalize().scale(amount));
-    //this.direction().y += amount;
-    this.view_matrix = this.lookAt();
+    //var forward = this.target.normalize().scale(amount);
+    this.position = this.position.subtract(new Vector(0,0,amount));
+    this.view_matrix = this.createViewMatrix();
 };
 /** @method */
 Camera.prototype.moveBackward = function(amount){
-    // TODO: WRONG!!!
-    this.position = this.position.subtract(this.direction().normalize().scale(amount));
-    //this.direction().y += amount;
-    this.view_matrix = this.lookAt();
-};
-Camera.prototype.rollLeft = function(amount){
-    this.rotation.roll += amount;
-    this.up.x = Math.sin(this.rotation.roll);
-    this.up.y = -Math.cos(this.rotation.roll);
-    this.up.z = 0;
-    this.view_matrix = this.lookAt();
-};
-Camera.prototype.rollRight = function(amount){
-    this.rotation.roll -= amount;
-    this.up.x = Math.sin(this.rotation.roll);
-    this.up.y = -Math.cos(this.rotation.roll);
-    this.up.z = 0;
-    this.view_matrix = this.lookAt();
-};
-/** @method */
-Camera.prototype.orbitTo = function(){
-
-};
-/** @method */
-Camera.prototype.zoomIn = function(amount){
-    // TODO: WRONG!!!
-    this.position.z += amount;
-    //this.direction().z += amount;
-    this.view_matrix = this.lookAt();
-};
-/** @method */
-Camera.prototype.zoomOut = function(amount){
-    // TODO: WRONG!!!
-    this.position.z -= amount;
-    //this.direction().z += amount;
-    this.view_matrix = this.lookAt();
-};
-/** @method */
-Camera.prototype.orbitLeft = function(){
-
-};
-/** @method */
-Camera.prototype.orbitRight = function(){
-
+    this.position = this.position.add(new Vector(0,0,amount));
+    this.view_matrix = this.createViewMatrix();
 };
 
 module.exports = Camera;
