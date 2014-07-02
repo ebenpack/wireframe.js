042667eea9f0af4f64e3a5687f88d39ed318755b
ebenpack <ebenpackwood@gmail.com>
2014-07-02 16:50:00 -0400

    Remove mesh normal

--- src/math/mesh.js
+++ src/math/mesh.js
@@ -15,42 +15,7 @@ function Mesh(name, vertices, faces){
     this.position = new Vector(0, 0, 0);
     this.rotation = {'yaw': 0, 'pitch': 0, 'roll': 0};
     this.scale = {'x': 1, 'y': 1, 'z': 1};
-    for (var i = 0; i < this.faces.length; i++){
-        var face = this.faces[i];
-        var a = this.vertices[face.face[0]];
-        var b = this.vertices[face.face[1]];
-        var c = this.vertices[face.face[2]];
-        this.faces[i].normal = Mesh.normal(a,b,c);
-        this.faces[i].centroid = Mesh.centroid(a,b,c);
-    }
 }
-/**
- * Returns the centroid vector for the given face.
- * @param  {Vector} a
- * @param  {Vector} b
- * @param  {Vector} c
- * @return {Vector}
- */
-Mesh.centroid = function(a, b, c){
-    return new Vector((a.x + b.x + c.x) / 3, (a.y + b.y + c.y) / 3, (a.z + b.z + c.z) / 3);
-};
-/**
- * Returns the normal vector for a face with the given vertices.
- * @param  {Vector} a
- * @param  {Vector} b
- * @param  {Vector} c
- * @return {Vector}
- */
-Mesh.normal = function(a, b, c){
-    var side1 = b.subtract(a);
-    var side2 = c.subtract(a);
-    var norm = side1.cross(side2);
-    if (norm.magnitude() <= 0.00000001){
-        return norm;
-    } else {
-        return norm.normalize();
-    }
-};
 Mesh.fromJSON = function(json){
     var vertices = [];
     var faces = [];
