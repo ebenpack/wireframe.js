d0344a8ea14f66feb35b71e6a666ef3b1d2fa05e
ebenpack <ebenpackwood@gmail.com>
2014-07-03 10:35:48 -0400

    Stub out event documentation

--- src/engine/events.js
+++ src/engine/events.js
@@ -10,46 +10,42 @@ function EventTarget(){
     this._listeners = {};
 }
 
-EventTarget.prototype = {
+/** @method */
+EventTarget.prototype.addListener = function(type, listener){
+    if (typeof this._listeners[type] === "undefined"){
+        this._listeners[type] = [];
+    }
 
-    constructor: EventTarget,
+    this._listeners[type].push(listener);
+};
+/** @method */
+EventTarget.prototype.fire = function(event){
+    if (typeof event === "string"){
+        event = { type: event };
+    }
+    if (!event.target){
+        event.target = this;
+    }
 
-    addListener: function(type, listener){
-        if (typeof this._listeners[type] === "undefined"){
-            this._listeners[type] = [];
+    if (!event.type){  //falsy
+        throw new Error("Event object missing 'type' property.");
+    }
+
+    if (this._listeners[event.type] instanceof Array){
+        var listeners = this._listeners[event.type];
+        for (var i=0, len=listeners.length; i < len; i++){
+            listeners[i].call(this, event);
         }
-
-        this._listeners[type].push(listener);
-    },
-
-    fire: function(event){
-        if (typeof event === "string"){
-            event = { type: event };
-        }
-        if (!event.target){
-            event.target = this;
-        }
-
-        if (!event.type){  //falsy
-            throw new Error("Event object missing 'type' property.");
-        }
-
-        if (this._listeners[event.type] instanceof Array){
-            var listeners = this._listeners[event.type];
-            for (var i=0, len=listeners.length; i < len; i++){
-                listeners[i].call(this, event);
-            }
-        }
-    },
-
-    removeListener: function(type, listener){
-        if (this._listeners[type] instanceof Array){
-            var listeners = this._listeners[type];
-            for (var i=0, len=listeners.length; i < len; i++){
-                if (listeners[i] === listener){
-                    listeners.splice(i, 1);
-                    break;
-                }
+    }
+};
+/** @method */
+EventTarget.prototype.removeListener = function(type, listener){
+    if (this._listeners[type] instanceof Array){
+        var listeners = this._listeners[type];
+        for (var i=0, len=listeners.length; i < len; i++){
+            if (listeners[i] === listener){
+                listeners.splice(i, 1);
+                break;
             }
         }
     }
