ab0dd74dd19fdeb66044416c885bf6641f0f4b95
ebenpack <ebenpackwood@gmail.com>
2014-07-06 12:42:01 -0400

    Add matrix test stubs

--- tests/math/matrix.js
+++ tests/math/matrix.js
@@ -7,5 +7,56 @@ describe('Matrix', function(){
             var matrix = new Matrix();
             assert.equal(16, matrix.length);
         })
-    })
+    });
+    describe('equal', function(){
+
+    });
+    describe('add', function(){
+        
+    });
+    describe('subtract', function(){
+        
+    });
+    describe('multiplyScalar', function(){
+        
+    });
+    describe('multiply', function(){
+        
+    });
+    describe('negate', function(){
+        
+    });
+    describe('transpose', function(){
+        
+    });
+    describe('rotationX', function(){
+        
+    });
+    describe('rotationY', function(){
+        
+    });
+    describe('rotationZ', function(){
+        
+    });
+    describe('rotationAxis', function(){
+        
+    });
+    describe('rotation', function(){
+        
+    });
+    describe('translation', function(){
+        
+    });
+    describe('scale', function(){
+        
+    });
+    describe('identity', function(){
+        
+    });
+    describe('zero', function(){
+        
+    });
+    describe('fromArray', function(){
+        
+    });
 });
