e527ffae81e1cfa001d25633a5685a88e7c072d4
ebenpack <ebenpackwood@gmail.com>
2014-07-08 16:44:50 -0400

    Add matrix tests

--- tests/math/matrix.js
+++ tests/math/matrix.js
@@ -2,26 +2,76 @@ var Matrix = require('../../src/math/matrix.js');
 var assert = require("assert");
 
 suite('Matrix', function(){
-    var zero, identity;
+    var zero, zero2, identity, identity2, ones, m0, m1, m2, m3, m4, m5;
     setup(function(){
-        zero = new Matrix();
+        zero = Matrix.zero();
         identity = Matrix.identity();
+        zero2 = new Matrix();
+        identity2 = new Matrix();
+        identity2[0] = 1;
+        identity2[5] = 1;
+        identity2[10] = 1;
+        identity2[15] = 1;
+        ones = new Matrix();
+        m0 = new Matrix();
+        m1 = new Matrix();
+        m2 = new Matrix();
+        m3 = new Matrix();
+        m4 = new Matrix();
+        m5 = Matrix.fromArray([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]);
+        m4[0] = 0;
+        m4[1] = 1;
+        m4[2] = 1;
+        m4[3] = 2;
+        m4[4] = 3;
+        m4[5] = 5;
+        m4[6] = 8;
+        m4[7] = 13;
+        m4[8] = 21;
+        m4[9] = 34;
+        m4[10] = 55;
+        m4[11] = 89;
+        m4[12] = 144;
+        m4[13] = 233;
+        m4[14] = 377;
+        m4[15] = 610;
+        for (var i = 0; i < 16; i++){
+            ones[i] = 1;
+            m0[i] = i;
+            m1[i] = i+1;
+            m2[i] = i+2;
+            m3[i] = i*2;
+        }
     });
     suite('properties', function(){
         test('length', function(){
-            assert.equal(16, zero.length);
-            assert.equal(16, identity.length);
+            assert.equal(zero.length, 16);
+            assert.equal(identity.length, 16);
+            assert.equal(zero2.length, 16);
+            assert.equal(identity2.length, 16);
+            assert.equal(m1.length, 16);
+            assert.equal(m2.length, 16);
+            assert.equal(m3.length, 16);
         });
     });
     suite('methods', function(){
         test('equal', function(){
-            
+            assert.ok(identity.equal(identity2));
+            assert.ok(zero.equal(zero2));
+            assert.ok(!identity.equal(zero));
+            assert.ok(m4.equal(m5));
+            assert.ok(!m0.equal(m1));
         });
         test('add', function(){
-            
+            var t1 = zero.add(m1);
+            var t2 = m0.add(ones);
+            assert.ok(t1.equal(m1));
+            assert.ok(t2.equal(m1));
         });
         test('subtract', function(){
-            
+            //var t1 = 0;
+            var t2 = m1.subtract(ones);
+            assert.ok(t2.equal(m0));
         });
         test('multiplyScalar', function(){
             
