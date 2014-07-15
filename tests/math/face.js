213257f7129872610b7b4b8d53a45040ba11f708
ebenpack <ebenpackwood@gmail.com>
2014-07-15 14:25:47 -0400

    Add face and mesh tests

--- tests/math/face.js
+++ tests/math/face.js
@@ -9,11 +9,13 @@ suite('Face', function(){
         face = new Face(0, 1, 2, "red");
     });
     suite('properties', function(){
+        test('vertices', function(){
+            assert.equal(face.face[0], 0);
+            assert.equal(face.face[1], 1);
+            assert.equal(face.face[2], 2);
+        });
         test('color', function(){
             assert.equal(face.color.rgb.r, 255);
         });
-    });
-    suite('methods', function(){
-
     });
 });
